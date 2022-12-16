ba8effe93715237457095b3794188fd928f1bfa9
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2022-12-15 18:05:48 -0800

    Print this statement

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	/*Print this comment*/
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	/*Return Success*/
+	return (0);
+}
