21761f300afe27319386aae1fda8c65fdf848391
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2022-12-17 20:41:36 -0800

    Print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,9 +7,10 @@
  */
 int main(void)
 {
-	int digit;
+	int i;
 
-	for (digit = 0; digit < 10; digit++)
-		printf("%d\n", digit);
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
 	return (0);
 }
