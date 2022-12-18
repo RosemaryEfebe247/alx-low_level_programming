d4723cbd9ccf13036bd9112d5da2c01bfeaea84f
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2022-12-18 09:52:40 -0800

    Print alpha 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet x10
+ *
+ * Return void
+ */
+void print_alphabet_x10(void)
+{
+	int count;
+	char a;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+			_putchar(a);
+	}
+	_putchar('\n');
+}
