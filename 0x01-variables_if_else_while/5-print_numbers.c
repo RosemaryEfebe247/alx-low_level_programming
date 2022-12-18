b8f636aeff60045f0f84ccabd4f6c849ca55ea4b
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2022-12-17 20:30:32 -0800

    digit of number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print digit
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char digit;
+
+	for (digit = 0; digit <= 9; digit++)
+		printf("%d\n", digit);
+	return (0);
+}
