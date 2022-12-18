13c28455e9a209e430ac6b052d429fd684f94ace
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2022-12-18 09:03:12 -0800

    Create function to print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet using _putchar
+ *
+ * Return: Always void
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	if (a <= 'z')
+		_putchar(a);
+	_putchar('\n');
+}
