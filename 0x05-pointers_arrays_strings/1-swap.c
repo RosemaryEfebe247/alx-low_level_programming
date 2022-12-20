84acf39e4ab0ef89dc3eaf3e744f97f19e461c57
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2022-12-19 17:46:03 -0800

    Swap two variables

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,13 +6,11 @@
  * @b: another value to be swapped
  * Return: *a = *b
  */
-int swap_int(int *a, int *b)
+void swap_int(int *a, int *b)
 {
 	int t;
 
 	t = *a;
 	*a = *b;
 	*b = t;
-
-	return (*a = *b);
 }
