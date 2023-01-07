b9723194e2cb4bd3788155b00c27098e4b269492
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2023-01-07 13:58:42 -0800

    Print arg count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - The function to count arg
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
