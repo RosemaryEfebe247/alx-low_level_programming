2b5fc18294d7d2d7e332c9b8279a5f7a3f5832e5
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2023-01-07 12:27:22 -0800

    Print the name of program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
  * @argv: The pointer to the varaibles used
  * Return: Always 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-	printf("The program name is: %s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
