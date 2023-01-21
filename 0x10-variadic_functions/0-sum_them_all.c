fa2046b76f71fb32aeb2d2cca51ca49903aae2c7
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2023-01-21 13:05:33 -0800

    Sum of numbers

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - The variodic functon to sum positive num
+ * @n: The last fixed argument argN
+ * Return: Always 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum = 0;
+	va_list ptr;
+	va_start(ptr, n);
+
+	if (n == 0)
+		return (0);
+	for (i = 0; i < n; i++)
+	sum += va_arg(ptr, unsigned int);
+	va_end(ptr);
+	return (sum);
+}
