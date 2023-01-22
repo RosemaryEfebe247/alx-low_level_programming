f2add790c346db687f709bd4866049b95818a737
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2023-01-21 19:48:16 -0800

    Print absolute value

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#include <stdlib.h>
+/**
+ * ABS - Function to print absolute value
+ * @x: Argument
+ */
+#define ABS(x) (abs(int x))
+#endif
