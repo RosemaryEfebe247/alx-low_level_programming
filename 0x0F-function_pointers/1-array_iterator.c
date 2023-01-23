d9eb6553959fb89b6210376dc58b7936216de250
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2023-01-22 16:36:11 -0800

    Iterate the values in th array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - Function to use a function to iterate array
+ * @array: Array of elements
+ * @size: size of the array
+ * @action: pointer to the function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array != NULL && action != NULL)
+	for (i = 0; i < array[size]; i++)
+		(*action)(array[i]);
+}
