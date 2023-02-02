d510de7a414467da1e452c28ad29ece35115b3db
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2023-02-01 19:16:26 -0800

    Set head to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,18 +2,18 @@
 /**
  * free_listint2 - Function to free head and set it to null
  * @head: pointer to a head pointer
+ *
+ * Description: Sets the head to null
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (head == NULL)
-		return;
-	while (head)
+	while (head != NULL)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 	}
-	head = NULL;
+	*head = NULL;
 }
