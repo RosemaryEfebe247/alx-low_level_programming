251c0ce62d4c20f60b93e64600132a1e85a33677
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2023-02-03 16:15:15 -0800

    Convert from Binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <math.h>
-#include <stddef.h>
 /**
  * binary_to_uint - Convert Binary to Decimal
  * @b : pointer to the address of binary
@@ -11,16 +9,18 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0, mult = 1;
 	int len;
 
-	if (*b == '\0')
+	if (b == 0)
 		return (0);
 	for (len = 0; b[len];)
 		len++;
-	for (len -= 1; len >= 0; len--)
+	len = len - 1;
+	while (len >= 0)
 	{
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
 		num += (b[len] - '0') * mult;
 		mult *= 2;
+		len--;
 	}
 	return (num);
 }
