38e2abb6ec4886f00ec0087b9b9f45cb92e65ba5
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2023-02-03 16:01:04 -0800

    Convert from Binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,9 +17,9 @@ unsigned int binary_to_uint(const char *b)
 		len++;
 	for (len -= 1; len >= 0; len--)
 	{
-		if (b[len] != 0 && b[len] != 1)
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
-		num += (b[len] - 0) * mult;
+		num += (b[len] - '0') * mult;
 		mult *= 2;
 	}
 	return (num);
