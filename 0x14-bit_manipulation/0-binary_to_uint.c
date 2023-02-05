5f8bc6cc67722e4276fe8e7cb51902c2aef4b17a
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2023-02-05 18:40:51 +0000

    Convert Binarytodec

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,11 +9,12 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0, mult = 1;
 	int len;
 
-	if (*b == '\0')
+	if (b == 0)
 		return (0);
 	for (len = 0; b[len]; )
 		len++;
-	for (len -= 1; len >= 0; len--)
+	len = len - 1;
+	while (len >= 0)
 	{
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
